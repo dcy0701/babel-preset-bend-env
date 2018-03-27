93814fd864625fd70e5d76c41605d4f39810ad97
dongchongyang <dongchongyang@meituan.com>
2018-03-27 16:58:50 +0800

    docs: update readme

---
+++ index.js
@@ -0,0 +1,7 @@
+module.exports = {
+  plugins: [
+      require("babel-plugin-transform-decorators-legacy"),
+      require("babel-plugin-transform-class-properties"),
+      require("babel-plugin-bend-component"),
+  ]
+};
